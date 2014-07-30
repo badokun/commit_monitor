b8d6d499d35ab2644af0299505bb081f490fa768
Henno van Rensburg <henno_vanrensburg@monex.co.jp>
2014-07-30 15:24:24 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+var application_root = __dirname,
+    express = require("express"),
+    path = require("path");
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    console.log(req.body); // populated!
+    next();
+});
+
+app.post('/api', function (req, res) {
+    var name = req.body.name,
+        color = req.body.color;
+    res.send(200);
+    // ...
+});
+
+var server = app.listen(3000, function () {
+    console.log('Listening on port %d', server.address().port);
+});
+
+
